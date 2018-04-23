ae52441e92c7e7b2cc9efa2ccd738c91750e2c15
samaddur <samaddur@DIN51002431.corp.capgemini.com>
2018-04-23 14:34:42 +0530

    comitted changes

---
+++ Lab1=3/src/com/cg/Employee.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+public class Employee {
+	private String firstName; 
+	private String lastName; 
+	private double salary;
+	Date doj;
+	private String grade;
+	private int id;
+public void employeeDetails(String firstName,String lastName,double salary,String grade,Date doj)
+{
+	
+}
+}
